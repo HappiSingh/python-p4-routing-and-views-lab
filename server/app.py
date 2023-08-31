b04bffeec8efa6e75f27a83a3becb6d7811e9c99
Harpreet Singh <Harpreets1201@gmail.com>
2023-08-31 06:00:53 -0400

    Completed lab

--- server/app.py
+++ server/app.py
@@ -1,8 +1,42 @@
 #!/usr/bin/env python3
-
+import ipdb
 from flask import Flask
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    nums = ""
+    for num in range(parameter):
+        nums = nums + str(num) + '\n'
+    return nums
+    
+    
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    ans = 0
+    if operation == '+':
+        ans = num1 + num2
+    elif operation == '-':
+        ans = num1 - num2
+    elif operation == '*':
+        ans = num1 * num2
+    elif operation == 'div':
+        ans = num1 / num2
+    elif operation == '%':
+        ans = num1 % num2
+
+    return str(ans)
